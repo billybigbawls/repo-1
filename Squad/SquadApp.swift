def1eda942f9a61cb309d5848c38f754d7b16bbe
abdinoorx2 <abdinoorbusiness@gmail.com>
2024-10-25 05:07:44 -0400

    Initial Commit

---
+++ Squad/SquadApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquadApp.swift
+//  Squad
+//
+//  Created by Abdinoor Abdinoor on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SquadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SquadTests/SquadTests.swift
@@ -0,0 +1,17 @@
+//
+//  SquadTests.swift
+//  SquadTests
+//
+//  Created by Abdinoor Abdinoor on 10/25/24.
+//
+
+import Testing
+@testable import Squad
+
+struct SquadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
